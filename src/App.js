a54c0ee80f065821a7fe62b44e5550b26af23c74
versage <stephaneatontsa@yahoo.fr>
2023-09-26 18:43:57 +0200

    First commit Navbar test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import './App.css';
 
+
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/pages/home/Home";
+import About from "./components/pages/about/About";
+import Online from "./components/pages/online/Online";
+import Offline from "./components/pages/offline/Offline";
+import Contact from "./components/pages/contact/Contact";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Router basename="/">
+
+                {/* Add Menu Component */}
+                <Navbar />
+
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/About" component={About}/>
+                    <Route path="/Online" component={Online}/>
+                    <Route path="/Offline" component={Offline}/>
+                    <Route path="/Contact" component={Contact}/>
+
+
+                </Switch>
+            </Router>
+
+        </div>
+    );
 }
 
 export default App;
